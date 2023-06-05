cdfd72616c9dab10d0a559245fb985c2bc9c2ce0
rolando-sw <rolando.martinez@sellwhenever.com>
2023-06-05 14:31:15 -0400

    fix references
    remove some not required modules

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,9 +1,5 @@
 import fs from "fs";
 import Jimp = require("jimp");
-import { NextFunction } from 'connect';
-import { Request, Response } from 'express';
-import * as jwt from 'jsonwebtoken';
-import { config } from '../config/config';
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
